c4c9c73f0ec3d2362fcbcfbdc4df29a0b7baf070
borna.sumiga <sumiga.borna@gmail.com>
2021-03-09 13:50:14 +0100

    Done with video 34.

--- app/src/main/java/com/example/dagger2practiceapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/dagger2practiceapp/ui/main/MainActivity.kt
@@ -3,17 +3,30 @@ package com.example.dagger2practiceapp.ui.main
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.dagger2practiceapp.BaseActivity
 import com.example.dagger2practiceapp.R
 import com.example.dagger2practiceapp.ui.main.posts.PostsFragment
 import com.example.dagger2practiceapp.ui.main.profile.ProfileFragment
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout : DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        testFragment()
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        init()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,7 +44,25 @@ class MainActivity : BaseActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun testFragment(){
-        supportFragmentManager.beginTransaction().replace(R.id.mainContainer,PostsFragment()).commitAllowingStateLoss()
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_profile -> {
+                navController.navigate(R.id.profileScreen)
+            }
+            R.id.nav_posts -> {
+                navController.navigate(R.id.postsScreen)
+            }
+        }
+
+        item.isChecked = true
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun init(){
+        val navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(navigationView,navController)
+        navigationView.setNavigationItemSelectedListener(this)
     }
 }
